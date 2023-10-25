41cf278492bde80c50c76f195e162f83ce60792d
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-10-25 10:31:13 +0200

    Update .clang-format. Reformat.

--- main.cpp
+++ main.cpp
@@ -8,103 +8,102 @@
 
 namespace {
 
-    class counter {
-    public:
-        counter () = default;
-        counter (counter const & ) = delete;
-        counter (counter && ) = delete;
-        counter & operator= (counter const & ) = delete;
-        counter & operator= (counter && ) = delete;
+  class counter {
+  public:
+    counter () = default;
+    counter (counter const &) = delete;
+    counter (counter &&) = delete;
+    counter & operator= (counter const &) = delete;
+    counter & operator= (counter &&) = delete;
 
-        int increment ();
-        int count () const;
-        void wait_for_value (int v) const;
+    int increment ();
+    int count () const;
+    void wait_for_value (int v) const;
 
-    private:
-        mutable std::mutex mut_;
-        mutable std::condition_variable cv_;
+  private:
+    mutable std::mutex mut_;
+    mutable std::condition_variable cv_;
 
-        int count_ = 0;
-    };
+    int count_ = 0;
+  };
 
-    int counter::increment () {
-        std::lock_guard<std::mutex> _{mut_};
-        ++count_;
-        cv_.notify_one ();
-        return count_;
+  int counter::increment () {
+    std::lock_guard<std::mutex> _{mut_};
+    ++count_;
+    cv_.notify_one ();
+    return count_;
+  }
+
+  void counter::wait_for_value (int v) const {
+    std::unique_lock<std::mutex> lock{mut_};
+    while (count_ < v) {
+      cv_.wait (lock);
     }
+  }
 
-    void counter::wait_for_value (int v) const {
-        std::unique_lock<std::mutex> lock{mut_};
-        while (count_ < v) {
-            cv_.wait (lock);
-        }
-    }
-
-    int counter::count () const {
-        std::unique_lock<std::mutex> lock{mut_};
-        return count_;
-    }
+  int counter::count () const {
+    std::unique_lock<std::mutex> lock{mut_};
+    return count_;
+  }
 
 } // end anonymous namespace
 
 int main (int /*argc*/, char const * /*argv*/[]) {
-    using namespace pubsub;
+  using namespace pubsub;
 
-    channel<std::string> chan;
+  channel<std::string> chan;
 
-    counter listening_counter;
-    counter received_counter;
+  counter listening_counter;
+  counter received_counter;
 
-    std::mutex cout_mut;
-    constexpr auto num_subscribers = 3U;
-    constexpr auto num_messages = 100U;
-    std::array<counter, num_subscribers> per_subscriber_received_counter;
+  std::mutex cout_mut;
+  constexpr auto num_subscribers = 3U;
+  constexpr auto num_messages = 100U;
+  std::array<counter, num_subscribers> per_subscriber_received_counter;
 
-    using subscriber_ptr = std::unique_ptr<subscriber<std::string>>;
-    std::vector<std::tuple<subscriber_ptr, std::thread>> subscribers;
-    subscribers.reserve (num_subscribers);
+  using subscriber_ptr = std::unique_ptr<subscriber<std::string>>;
+  std::vector<std::tuple<subscriber_ptr, std::thread>> subscribers;
+  subscribers.reserve (num_subscribers);
 
-    for (auto sub_ctr = 0U; sub_ctr < num_subscribers; ++sub_ctr) {
-        subscriber_ptr ptr = chan.new_subscriber ();
-        std::thread thread{
-            [&](subscriber<std::string> * const sub, int id) {
-                listening_counter.increment ();
-                while (std::optional<std::string> const message = sub->wait ()) {
-                    {
-                        std::lock_guard<std::mutex> cout_lock{cout_mut};
-                        std::cout << "sub(" << id << "): " << *message << '\n';
-                    }
-                    received_counter.increment ();
-                    per_subscriber_received_counter [id].increment ();
-                }
-            },
-            ptr.get (), sub_ctr};
-        subscribers.emplace_back (std::move (ptr), std::move (thread));
-    }
+  for (auto sub_ctr = 0U; sub_ctr < num_subscribers; ++sub_ctr) {
+    subscriber_ptr ptr = chan.new_subscriber ();
+    std::thread thread{[&] (subscriber<std::string> * const sub, int id) {
+                         listening_counter.increment ();
+                         while (std::optional<std::string> const message = sub->wait ()) {
+                           {
+                             std::lock_guard<std::mutex> cout_lock{cout_mut};
+                             std::cout << "sub(" << id << "): " << *message << '\n';
+                           }
+                           received_counter.increment ();
+                           per_subscriber_received_counter[id].increment ();
+                         }
+                       },
+                       ptr.get (), sub_ctr};
+    subscribers.emplace_back (std::move (ptr), std::move (thread));
+  }
 
-    // Wait for our subscribers to get to the point that they're beginning to listen.
-    listening_counter.wait_for_value (num_subscribers);
+  // Wait for our subscribers to get to the point that they're beginning to listen.
+  listening_counter.wait_for_value (num_subscribers);
 
-    // Now post some messages to the channel.
-    for (auto message_ctr = 0U; message_ctr < num_messages; ++message_ctr) {
-        std::this_thread::sleep_for (std::chrono::milliseconds{message_ctr * 10});
-        std::ostringstream os;
-        os << "message " << message_ctr;
-        chan.publish (os.str ());
-    }
+  // Now post some messages to the channel.
+  for (auto message_ctr = 0U; message_ctr < num_messages; ++message_ctr) {
+    std::this_thread::sleep_for (std::chrono::milliseconds{message_ctr * 10});
+    std::ostringstream os;
+    os << "message " << message_ctr;
+    chan.publish (os.str ());
+  }
 
-    received_counter.wait_for_value (num_messages * num_subscribers);
+  received_counter.wait_for_value (num_messages * num_subscribers);
 
 #ifndef NDEBUG
-    for (auto & ctr : per_subscriber_received_counter) {
-        assert (ctr.count () == num_messages);
-    }
+  for (auto & ctr : per_subscriber_received_counter) {
+    assert (ctr.count () == num_messages);
+  }
 #endif
 
-    // Cancel the subscriptions and wait for the threads to complete.
-    for (auto & subscriber : subscribers) {
-        std::get<0> (subscriber)->cancel ();
-        std::get<1> (subscriber).join ();
-    }
+  // Cancel the subscriptions and wait for the threads to complete.
+  for (auto & subscriber : subscribers) {
+    std::get<0> (subscriber)->cancel ();
+    std::get<1> (subscriber).join ();
+  }
 }
--- pubsub.hpp
+++ pubsub.hpp
@@ -11,238 +11,254 @@
 
 namespace pubsub {
 
-    template <typename Message>
-    class channel;
+  template <typename Message>
+  class channel;
 
-    //*          _               _ _              *
-    //*  ____  _| |__ ___ __ _ _(_) |__  ___ _ _  *
-    //* (_-< || | '_ (_-</ _| '_| | '_ \/ -_) '_| *
-    //* /__/\_,_|_.__/__/\__|_| |_|_.__/\___|_|   *
-    //*                                           *
-    /// An instance of the subscriber class represents a subscription to messages published on an
-    /// associated owning channel.
-    template <typename Message>
-    class subscriber {
-        friend class channel<Message>;
-    public:
-        explicit constexpr subscriber (channel<Message> * const c) noexcept
-                : owner_{c} {
-            assert (c != nullptr);
-        }
-        virtual ~subscriber () noexcept;
+  //*          _               _ _              *
+  //*  ____  _| |__ ___ __ _ _(_) |__  ___ _ _  *
+  //* (_-< || | '_ (_-</ _| '_| | '_ \/ -_) '_| *
+  //* /__/\_,_|_.__/__/\__|_| |_|_.__/\___|_|   *
+  //*                                           *
+  /// An instance of the subscriber class represents a subscription to messages published on an
+  /// associated owning channel.
+  template <typename Message>
+  class subscriber {
+    friend class channel<Message>;
 
-        // No copying or assignment.
-        subscriber (subscriber const &) = delete;
-        subscriber (subscriber &&) noexcept = delete;
-        subscriber & operator= (subscriber const &) = delete;
-        subscriber & operator= (subscriber &&) noexcept = delete;
-
-        /// Blocks waiting for a message to be published on the owning channel of for the
-        /// subscription to be cancelled.
-        ///
-        /// \returns An optional holding a message published to the owning channel or with no value
-        /// indicating that the subscription has been cancelled.
-        std::optional<Message> wait ();
-
-        /// \param rel_time A duration representing the maximum time to spend waiting. Note that rel_time must be small enough not to overflow when added to std::chrono::steady_clock::now().
-        template <typename Rep, typename Period>
-        std::optional<Message> wait_for (std::chrono::duration<Rep, Period> const & rel_time);
-
-        /// \param timeout_time	-	An object of type std::chrono::time_point representing the time when to stop waiting.
-        template <typename Clock, typename Duration>
-        std::optional<Message> wait_until (std::chrono::time_point<Clock, Duration> const & timeout_time);
-
-        /// Cancels a subscription.
-        ///
-        /// The subscription is marked as inactive. If waiting it is woken up.
-        void cancel ();
-        [[nodiscard]] constexpr bool active () const noexcept { return active_; }
-
-        /// \returns A reference to the owning channel.
-        [[nodiscard]] channel<Message> & owner () noexcept { return *owner_; }
-        /// \returns A reference to the owning channel.
-        [[nodiscard]] channel<Message> const & owner () const noexcept { return *owner_; }
-
-    private:
-        /// The queue of published messages waiting to be delivered to a listening subscriber.
-        ///
-        /// \note This is a queue of strings. If there are multiple subscribers to this channel then
-        /// the strings will be duplicated in each which could be very inefficient. An alternative
-        /// would be to store shared_ptr<string>. For the moment I'm leaving it like this on the
-        /// assumption that there will typically be just a single subscriber.
-        std::queue<Message> queue_;
-
-        /// The channel with which this subscription is associated.
-        channel<Message> * const owner_;
-
-        /// Should this subscriber continue to listen to messages?
-        bool active_ = true;
-    };
-
-    //*     _                       _  *
-    //*  __| |_  __ _ _ _  _ _  ___| | *
-    //* / _| ' \/ _` | ' \| ' \/ -_) | *
-    //* \__|_||_\__,_|_||_|_||_\___|_| *
-    //*                                *
-    /// Messages can be written ("published") to a channel; there can be multiple "subscribers"
-    /// which will all receive notification of published messages.
-    template <typename Message>
-    class channel {
-        friend class subscriber<Message>;
-
-    public:
-        channel () = default;
-        ~channel () noexcept;
-
-        // No copying or assignment.
-        channel (channel const &) = delete;
-        channel (channel &&) = delete;
-        channel & operator= (channel const &) = delete;
-        channel & operator= (channel &&) = delete;
-
-        /// Broadcasts a message to all subscribers.
-        void publish (Message const & message);
-
-        /// Creates a new subscriber instance and attaches it to this channel.
-        std::unique_ptr<subscriber<Message>> new_subscriber ();
-
-        /// Cancels a subscription.
-        ///
-        /// The subscription is marked as inactive. If waiting it is woken up.
-        void cancel (subscriber<Message> & sub) const;
-
-    private:
-        template <typename WaitFunction, typename = std::enable_if_t<std::is_invocable_r_v<bool,  WaitFunction, std::condition_variable&, std::unique_lock<std::mutex>&>>>
-        std::optional<Message> wait (subscriber<Message> &sub, WaitFunction wfn);
-
-        /// Called when a subscriber is destructed to remove it from the subscribers list.
-        void remove_sub (subscriber<Message> * sub) noexcept;
-
-        mutable std::mutex mut_;
-        mutable std::condition_variable cv_;
-
-        /// All of the subscribers to this channel.
-        std::unordered_set<subscriber<Message> *> subscribers_;
-    };
-
-    //*          _               _ _              *
-    //*  ____  _| |__ ___ __ _ _(_) |__  ___ _ _  *
-    //* (_-< || | '_ (_-</ _| '_| | '_ \/ -_) '_| *
-    //* /__/\_,_|_.__/__/\__|_| |_|_.__/\___|_|   *
-    //*                                           *
-    // (dtor)
-    // ~~~~~~
-    template <typename Message>
-    subscriber<Message>::~subscriber () noexcept {
-      owner_->remove_sub (this);
+  public:
+    explicit constexpr subscriber (channel<Message> * const c) noexcept
+            : owner_{c} {
+      assert (c != nullptr);
     }
+    virtual ~subscriber () noexcept;
 
-    // wait
-    // ~~~~
-    template <typename Message>
-    std::optional<Message> subscriber<Message>::wait () {
-      return owner_->wait (*this, [](std::condition_variable &cv, std::unique_lock<std::mutex> &lock) { cv.wait(lock); return true; });
-    }
+    // No copying or assignment.
+    subscriber (subscriber const &) = delete;
+    subscriber (subscriber &&) noexcept = delete;
+    subscriber & operator= (subscriber const &) = delete;
+    subscriber & operator= (subscriber &&) noexcept = delete;
 
-    // wait for
-    // ~~~~~~~~
-    template <typename Message>
+    /// Blocks waiting for a message to be published on the owning channel of for the
+    /// subscription to be cancelled.
+    ///
+    /// \returns An optional holding a message published to the owning channel or with no value
+    /// indicating that the subscription has been cancelled.
+    std::optional<Message> wait ();
+
+    /// \param rel_time A duration representing the maximum time to spend waiting. Note that
+    /// rel_time must be small enough not to overflow when added to
+    /// std::chrono::steady_clock::now().
     template <typename Rep, typename Period>
-    std::optional<Message> subscriber<Message>::wait_for (std::chrono::duration<Rep, Period> const & rel_time) {
-      return this->wait_until (std::chrono::steady_clock::now() + rel_time);
-    }
+    std::optional<Message> wait_for (std::chrono::duration<Rep, Period> const & rel_time);
 
-    // wait until
-    // ~~~~~~~~~~
-    template <typename Message>
+    /// \param timeout_time	-	An object of type std::chrono::time_point representing the
+    /// time when to stop waiting.
     template <typename Clock, typename Duration>
-    std::optional<Message> subscriber<Message>::wait_until (std::chrono::time_point<Clock, Duration> const & timeout_time) {
-      return owner_->wait (*this, [&timeout_time] (std::condition_variable &cv, std::unique_lock<std::mutex> &lock) { return cv.wait_until(lock, timeout_time) ==  std::cv_status::no_timeout; });
+    std::optional<Message>
+    wait_until (std::chrono::time_point<Clock, Duration> const & timeout_time);
+
+    /// Cancels a subscription.
+    ///
+    /// The subscription is marked as inactive. If waiting it is woken up.
+    void cancel ();
+    [[nodiscard]] constexpr bool active () const noexcept { return active_; }
+
+    /// \returns A reference to the owning channel.
+    [[nodiscard]] channel<Message> & owner () noexcept { return *owner_; }
+    /// \returns A reference to the owning channel.
+    [[nodiscard]] channel<Message> const & owner () const noexcept { return *owner_; }
+
+  private:
+    /// The queue of published messages waiting to be delivered to a listening subscriber.
+    ///
+    /// \note This is a queue of strings. If there are multiple subscribers to this channel then
+    /// the strings will be duplicated in each which could be very inefficient. An alternative
+    /// would be to store shared_ptr<string>. For the moment I'm leaving it like this on the
+    /// assumption that there will typically be just a single subscriber.
+    std::queue<Message> queue_;
+
+    /// The channel with which this subscription is associated.
+    channel<Message> * const owner_;
+
+    /// Should this subscriber continue to listen to messages?
+    bool active_ = true;
+  };
+
+  //*     _                       _  *
+  //*  __| |_  __ _ _ _  _ _  ___| | *
+  //* / _| ' \/ _` | ' \| ' \/ -_) | *
+  //* \__|_||_\__,_|_||_|_||_\___|_| *
+  //*                                *
+  /// Messages can be written ("published") to a channel; there can be multiple "subscribers"
+  /// which will all receive notification of published messages.
+  template <typename Message>
+  class channel {
+    friend class subscriber<Message>;
+
+  public:
+    channel () = default;
+    ~channel () noexcept;
+
+    // No copying or assignment.
+    channel (channel const &) = delete;
+    channel (channel &&) = delete;
+    channel & operator= (channel const &) = delete;
+    channel & operator= (channel &&) = delete;
+
+    /// Broadcasts a message to all subscribers.
+    void publish (Message const & message);
+
+    /// Creates a new subscriber instance and attaches it to this channel.
+    std::unique_ptr<subscriber<Message>> new_subscriber ();
+
+    /// Cancels a subscription.
+    ///
+    /// The subscription is marked as inactive. If waiting it is woken up.
+    void cancel (subscriber<Message> & sub) const;
+
+  private:
+    template <typename WaitFunction,
+              typename = std::enable_if_t<std::is_invocable_r_v<
+                bool, WaitFunction, std::condition_variable &, std::unique_lock<std::mutex> &>>>
+    std::optional<Message> wait (subscriber<Message> & sub, WaitFunction wfn);
+
+    /// Called when a subscriber is destructed to remove it from the subscribers list.
+    void remove_sub (subscriber<Message> * sub) noexcept;
+
+    mutable std::mutex mut_;
+    mutable std::condition_variable cv_;
+
+    /// All of the subscribers to this channel.
+    std::unordered_set<subscriber<Message> *> subscribers_;
+  };
+
+  //*          _               _ _              *
+  //*  ____  _| |__ ___ __ _ _(_) |__  ___ _ _  *
+  //* (_-< || | '_ (_-</ _| '_| | '_ \/ -_) '_| *
+  //* /__/\_,_|_.__/__/\__|_| |_|_.__/\___|_|   *
+  //*                                           *
+  // (dtor)
+  // ~~~~~~
+  template <typename Message>
+  subscriber<Message>::~subscriber () noexcept {
+    owner_->remove_sub (this);
+  }
+
+  // wait
+  // ~~~~
+  template <typename Message>
+  std::optional<Message> subscriber<Message>::wait () {
+    return owner_->wait (*this,
+                         [] (std::condition_variable & cv, std::unique_lock<std::mutex> & lock) {
+                           cv.wait (lock);
+                           return true;
+                         });
+  }
+
+  // wait for
+  // ~~~~~~~~
+  template <typename Message>
+  template <typename Rep, typename Period>
+  std::optional<Message>
+  subscriber<Message>::wait_for (std::chrono::duration<Rep, Period> const & rel_time) {
+    return this->wait_until (std::chrono::steady_clock::now () + rel_time);
+  }
+
+  // wait until
+  // ~~~~~~~~~~
+  template <typename Message>
+  template <typename Clock, typename Duration>
+  std::optional<Message>
+  subscriber<Message>::wait_until (std::chrono::time_point<Clock, Duration> const & timeout_time) {
+    return owner_->wait (
+      *this, [&timeout_time] (std::condition_variable & cv, std::unique_lock<std::mutex> & lock) {
+        return cv.wait_until (lock, timeout_time) == std::cv_status::no_timeout;
+      });
+  }
+
+  // cancel
+  // ~~~~~~
+  template <typename Message>
+  void subscriber<Message>::cancel () {
+    this->owner ().cancel (*this);
+  }
+
+
+  //*     _                       _  *
+  //*  __| |_  __ _ _ _  _ _  ___| | *
+  //* / _| ' \/ _` | ' \| ' \/ -_) | *
+  //* \__|_||_\__,_|_||_|_||_\___|_| *
+  //*                                *
+  // (dtor)
+  // ~~~~~~
+  template <typename Message>
+  channel<Message>::~channel () noexcept {
+    // Check that this channel has no subscribers.
+    assert (subscribers_.empty ());
+  }
+
+  // publish
+  // ~~~~~~~
+  template <typename Message>
+  void channel<Message>::publish (Message const & message) {
+    std::lock_guard<std::mutex> _{mut_};
+    if (subscribers_.size () > 0) {
+      for (auto & sub : subscribers_) {
+        sub->queue_.push (message);
+      }
+      cv_.notify_all ();
     }
+  }
 
-    // cancel
-    // ~~~~~~
-    template <typename Message>
-    void subscriber<Message>::cancel () {
-      this->owner ().cancel (*this);
+  // new subscriber
+  // ~~~~~~~~~~~~~~
+  template <typename Message>
+  std::unique_ptr<subscriber<Message>> channel<Message>::new_subscriber () {
+    std::lock_guard<std::mutex> lock{mut_};
+    auto resl = std::make_unique<subscriber<Message>> (this);
+    subscribers_.insert (resl.get ());
+    return resl;
+  }
+
+  // cancel
+  // ~~~~~~
+  template <typename Message>
+  void channel<Message>::cancel (subscriber<Message> & sub) const {
+    if (&sub.owner () == this) {
+      std::unique_lock<std::mutex> lock{mut_};
+      sub.active_ = false;
+      cv_.notify_all ();
     }
+  }
 
+  // remove sub
+  // ~~~~~~~~~~
+  template <typename Message>
+  void channel<Message>::remove_sub (subscriber<Message> * sub) noexcept {
+    std::lock_guard<std::mutex> _{mut_};
+    assert (subscribers_.find (sub) != subscribers_.end ());
+    subscribers_.erase (sub);
+  }
 
-    //*     _                       _  *
-    //*  __| |_  __ _ _ _  _ _  ___| | *
-    //* / _| ' \/ _` | ' \| ' \/ -_) | *
-    //* \__|_||_\__,_|_||_|_||_\___|_| *
-    //*                                *
-    // (dtor)
-    // ~~~~~~
-    template <typename Message>
-    channel<Message>::~channel () noexcept {
-        // Check that this channel has no subscribers.
-        assert (subscribers_.empty ());
-    }
-
-    // publish
-    // ~~~~~~~
-    template <typename Message>
-    void channel<Message>::publish (Message const & message) {
-        std::lock_guard<std::mutex> _{mut_};
-        if (subscribers_.size () > 0) {
-            for (auto & sub : subscribers_) {
-                sub->queue_.push (message);
-            }
-            cv_.notify_all ();
+  // listen
+  // ~~~~~~
+  template <typename Message>
+  template <typename WaitFunction, typename>
+  std::optional<Message> channel<Message>::wait (subscriber<Message> & sub, WaitFunction wfn) {
+    std::unique_lock<std::mutex> lock{mut_};
+    while (sub.active ()) {
+      if (sub.queue_.empty ()) {
+        if (!wfn (std::ref (cv_), std::ref (lock))) {
+          return {}; // timeout
         }
+      } else {
+        auto const message = std::move (sub.queue_.front ());
+        sub.queue_.pop ();
+        return message;
+      }
     }
-
-    // new subscriber
-    // ~~~~~~~~~~~~~~
-    template <typename Message>
-    std::unique_ptr<subscriber<Message>> channel<Message>::new_subscriber () {
-        std::lock_guard<std::mutex> lock{mut_};
-        auto resl = std::make_unique<subscriber<Message>>(this);
-        subscribers_.insert (resl.get ());
-        return resl;
-    }
-
-    // cancel
-    // ~~~~~~
-    template <typename Message>
-    void channel<Message>::cancel (subscriber<Message> & sub) const {
-        if (&sub.owner () == this) {
-            std::unique_lock<std::mutex> lock{mut_};
-            sub.active_ = false;
-            cv_.notify_all ();
-        }
-    }
-
-    // remove sub
-    // ~~~~~~~~~~
-    template <typename Message>
-    void channel<Message>::remove_sub (subscriber<Message> * sub) noexcept {
-        std::lock_guard<std::mutex> _{mut_};
-        assert (subscribers_.find (sub) != subscribers_.end ());
-        subscribers_.erase (sub);
-    }
-
-    // listen
-    // ~~~~~~
-    template <typename Message>
-    template <typename WaitFunction, typename>
-    std::optional<Message> channel<Message>::wait (subscriber<Message> & sub, WaitFunction wfn) {
-        std::unique_lock<std::mutex> lock{mut_};
-        while (sub.active ()) {
-            if (sub.queue_.empty ()) {
-                if (!wfn (std::ref(cv_), std::ref(lock))) {
-                  return {}; // timeout
-                }
-            } else {
-                auto const message = std::move (sub.queue_.front ());
-                sub.queue_.pop ();
-                return message;
-            }
-        }
-        return {};
-    }
+    return {};
+  }
 
 } // end namespace pubsub
 
